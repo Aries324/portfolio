09f8fcbc910e9b7d6bd9eae72cbe236af489d48b
Tiffany McLean <tamclean324@gmail.com>
2020-07-31 14:37:08 -0400

    adds form

--- src/components/contactus/contactus.js
+++ src/components/contactus/contactus.js
@@ -23,36 +23,76 @@ export default class ContactUs extends Component {
           <div className="row">
             <div className="eight columns">
               {/* form */}
-              <form name="contact" method="POST" data-netlify="true">
-                <p>
-                  <label>
-                    Your Name: <input type="text" name="name" />
-                  </label>
-                </p>
-                <p>
-                  <label>
-                    Your Email: <input type="email" name="email" />
-                  </label>
-                </p>
-                <p>
-                  <label>
-                    Your Role:{" "}
-                    <select name="role[]" multiple>
-                      <option value="leader">Leader</option>
-                      <option value="follower">Follower</option>
-                    </select>
-                  </label>
-                </p>
-                <p>
-                  <label>
-                    Message: <textarea name="message"></textarea>
-                  </label>
-                </p>
-                <p>
-                  <button type="submit">Send</button>
-                </p>
-              </form>
+              <form
+                name="contactForm"
+                id="contactForm"
+                method="POST"
+               
+                data-netlify="true"
+              >
+                <fieldset>
+                  <div>
+                    <label>
+                      Name <span className="required">*</span>
+                    </label>
+                    <input
+                      type="text"
+                      size={35}
+                      id="contactName"
+                      name="contactName"
+                    />
+                  </div>
+                  <div>
+                    <label>
+                      Email <span className="required">*</span>
+                    </label>
+                    <input
+                      type="text"
+                      size={35}
+                      id="contactEmail"
+                      name="contactEmail"
+                    />
+                  </div>
+                  <div>
+                    <label>Subject</label>
+                    <input
+                      type="text"
+                      size={35}
+                      id="contactSubject"
+                      name="contactSubject"
+                    />
+                  </div>
+                  <div>
+                    <label>
+                      Message <span className="required">*</span>
+                    </label>
+                    <textarea
+                      cols={50}
+                      rows={15}
+                      id="contactMessage"
+                      name="contactMessage"
+                    />
+                  </div>
+                  <div>
+                    <button className="submit">Submit</button>
+                    <span id="image-loader">
+                      <img alt="" src="images/loader.gif" />
+                    </span>
+                  </div>
+                </fieldset>
+              </form>{" "}
               {/* Form End */}
+              {/* contact-warning */}
+              <div id="message-warning">
+                {" "}
+                An error has occurred while processing your message
+              </div>
+              {/* contact-success */}
+              <div id="message-success">
+                <i className="fa fa-check" />
+                Your message was sent, thank you!
+                <br />
+              </div>
             </div>
             <aside className="four columns footer-widgets">
               <div className="widget widget_contact">
@@ -100,6 +140,8 @@ export default class ContactUs extends Component {
             <button type="submit">Send</button>
           </p>
         </form>
+
+        
       </React.Fragment>
     );
   }
